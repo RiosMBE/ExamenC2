04fa9d4ec8f9ad65d582eb2b40c6cb830a5faa7d
Brian Rios <briosm0601@gmail.com>
2023-03-06 17:16:44 -0700

    Crear proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const bodyparser = require("body-parser");
+const misRutas = require("./router/index");
+const path = require("path");
+
+const app = express();
+app.set('view engine', "ejs")
+app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile)
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(misRutas)
+
+const puerto = 603;
+
+app.use((req, res, next) =>{
+    res.status(404).sendFile(__dirname + '/public/error.html');
+})
+
+app.listen(puerto, ()=>{
+    console.log("iniciando puerto 603")
+})
---
+++ router/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const bodyparser = require("body-parser");
+
+module.exports = router;
+
+router.get("/index", (req, res)=>{
+    const valores = {
+        numRecibo: req.query.numRecibo,
+        nombre: req.query.nombre,
+        domicilio: req.query.domicilio,
+        servicio: req.query.servicio,
+        consumo: req.query.consumo
+    }
+    res.render('index.ejs', valores)
+})
+
+router.post("/index", (req, res)=>{
+    const valores = {
+        numRecibo: req.body.numRecibo,
+        nombre: req.body.nombre,
+        domicilio: req.body.domicilio,
+        servicio: req.body.servicio,
+        consumo: req.body.consumo
+    }
+    res.render('index.ejs', valores)
+})
+
+function btnLimpiar(){
+    document.getElementById("numRecibo").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("domicilio").value = "";
+    document.getElementById("servicio").value = "";
+    document.getElementById("consumo").value = "";
+}
